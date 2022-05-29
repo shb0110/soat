20c4bd567ff06f51752fbb721747a9ab1cc6a775
shb0110 <99003926+shb0110@users.noreply.github.com>
2022-05-29 23:49:46 +0500

    Add files via upload

---
+++ js.js
@@ -0,0 +1,26 @@
+setInterval(setAsosiy, 1000)
+
+
+
+const milSoat = document.querySelector('[bu-soat-mili]')
+const milMinut = document.querySelector('[bu-minut-mili]')
+const milSekund = document.querySelector('[bu-sekund-mili]')
+
+function setAsosiy() {
+    const currentDate = new Date()
+    const secondsRatio = currentDate.getSeconds()
+    const minutesRatio = (secondsRatio + currentDate.getMinutes())
+    const hoursRatio = (minutesRatio + currentRatio.getHours())
+    setRotation(milSekund, secondsRatio)
+    setRotation(milMinut, minutesRatio)
+    setRotation(milSoat, hoursRatio)
+
+}
+
+function setRotation(element, rotationRatio) {
+    element.style.setProperty('--rotation', rotationRatio * 360)
+
+}
+setAsosiy()
+
+
